b80fd82e3a2caa3d443e79011f48154216458d0a
wynjones1 <stuart@wynjones1.com>
2015-05-22 01:22:13 +0100

    nm

--- src/kernels/pass_through.cl
+++ src/kernels/pass_through.cl
@@ -1,4 +1,4 @@
 __kernel void pass_through(__global int *a, __global int *b)
 {
-	*b = *a;
+	*b = a[0] * a[0];
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main()
 {
 	cl_device_id     device_id     = NULL;
 	cl_context       context       = NULL;
-	cl_command_queue command_queue = NULL;
+	cl_command_queue cq            = NULL;
 	cl_program       program       = NULL;
 	cl_platform_id   platform_id   = NULL;
 	cl_int           ret           = 0;
@@ -17,7 +17,7 @@ int main()
 	std::vector<cl_kernel> kernels;
 	std::vector<cl_mem>    mem_objs;
 
-	setup(&context, &platform_id, &device_id, &command_queue);
+	setup(&context, &platform_id, &device_id, &cq);
 
 	/* Create Kernel Program from the source */
 	program = create_program_from_file(context, KERNEL_PATH "/pass_through.cl");
@@ -32,16 +32,31 @@ int main()
 	clSetKernelArg(kernels[0], 0, sizeof(int), (void*)&a);
 	clSetKernelArg(kernels[0], 0, sizeof(int), (void*)&b);
 #else
-	int a = 12;
-	int b;
+	int a = 154;
 	mem_objs.push_back(clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(a), &a, &ret));
 	mem_objs.push_back(clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(a), NULL, &ret));
 
 	clSetKernelArg(kernels[0], 0, sizeof(mem_objs[0]), &mem_objs[0]);
 	clSetKernelArg(kernels[0], 1, sizeof(mem_objs[1]), &mem_objs[1]);
+
+	for (int i = 0; i < 100; i++)
+	{
+		size_t work_size = 1;
+		clEnqueueWriteBuffer(cq, mem_objs[0], CL_TRUE, 0, sizeof(int), &i, 0, NULL, NULL);
+
+#if 0
+		clEnqueueTask(cq, kernels[0], 0, NULL, NULL);
+#else
+		clEnqueueNDRangeKernel(cq, kernels[0], 1, &work_size, &work_size, &work_size, 0, NULL, NULL);
+#endif
+
+		int *b = (int*)clEnqueueMapBuffer(cq, mem_objs[1], CL_TRUE, CL_MAP_READ, 0, sizeof(int), 0, NULL, NULL, &ret);
+		std::cout << "Value of b = " << *b << std::endl;
+		clEnqueueUnmapMemObject(cq, mem_objs[1], b, 0, NULL, NULL);
+	}
 #endif
 
 	/* Finalization */
-	cleanup(command_queue, kernels, mem_objs, program, context);
+	cleanup(cq, kernels, mem_objs, program, context);
 	return 0;
 }
